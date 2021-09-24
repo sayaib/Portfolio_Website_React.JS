1c5f78e0ca2caabb89c2517a2d5e0272ff7b523a
Sayaib Sarkar <66420972+sayaib@users.noreply.github.com>
2021-09-24 19:57:50 +0530

    update navbar iteam

---
+++ src/webpage_component/Manu.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import './style.css'
+
+const Manu = () => {
+  return (
+    <>
+      <div class="rc_nav" id="centered_nav">
+        <a href="#home" title="Home">
+          Home
+        </a>
+        <a href="#services" title="Services">
+          Services
+        </a>
+        <a href="#about" title="About">
+          About
+        </a>
+        <a href="#contact" title="Contact">
+          Contact
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Manu;
