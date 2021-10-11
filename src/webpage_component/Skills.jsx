1a87cf7aab55671264808884e0dcba5ded87cb0c
Sayaib Sarkar <66420972+sayaib@users.noreply.github.com>
2021-10-11 22:38:24 +0530

    update my skill section progress bar

--- src/webpage_component/Skills.jsx
+++ src/webpage_component/Skills.jsx
@@ -4,13 +4,100 @@ import "./style.css";
 const Skills = () => {
   return (
     <>
-      <div id="webpage">
-        <div id="webpage_body">
-          <h1 id="title" data-aos="fade-right">
-            SAYAIB SARKAR
-          </h1>
+      <body>
+        <div class="skills">
+          <p class="skl">MY SKILLS :</p>
+          <p class="skl sklLine">_________</p>
+          <div class="htmlProg ProgWidth">
+            <div class="p">
+              <p>html</p>
+              <p>95%</p>
+            </div>
+            <div class="pp">
+              <div class="skillDiv">
+                <span class="skillBar htmlBar"></span>
+              </div>
+            </div>
+          </div>
+          <div class="cssProg ProgWidth">
+            <div class="p">
+              <p>Css3</p>
+              <p>70%</p>
+            </div>
+            <div class="pp">
+              <div class="skillDiv">
+                <span class="skillBar cssBar"></span>
+              </div>
+            </div>
+          </div>
+          <div class="jsProg ProgWidth">
+            <div class="p">
+              <p>JavaScript</p>
+              <p>75%</p>
+            </div>
+            <div class="pp">
+              <div class="skillDiv">
+                <span class="skillBar jsBar"></span>
+              </div>
+            </div>
+          </div>
+          <div class="javaProg ProgWidth">
+            <div class="p">
+              <p>Java</p>
+              <p>85%</p>
+            </div>
+            <div class="pp">
+              <div class="skillDiv">
+                <span class="skillBar javaBar"></span>
+              </div>
+            </div>
+          </div>
+          <div class="c/c++Prog ProgWidth">
+            <div class="p">
+              <p>C/C++</p>
+              <p>80%</p>
+            </div>
+            <div class="pp">
+              <div class="skillDiv">
+                <span class="skillBar ccpBar"></span>
+              </div>
+            </div>
+          </div>
+          <div class="reactProg ProgWidth">
+            <div class="p">
+              <p>React</p>
+              <p>70%</p>
+            </div>
+            <div class="pp">
+              <div class="skillDiv">
+                <span class="skillBar reactBar"></span>
+              </div>
+            </div>
+          </div>
+          <div class="nodeProg ProgWidth">
+            <div class="p">
+              <p>Node.js</p>
+              <p>35%</p>
+            </div>
+            <div class="pp">
+              <div class="skillDiv">
+                <span class="skillBar nodeBar"></span>
+              </div>
+            </div>
+          </div>
+          <div class="sqlProg ProgWidth">
+            <div class="p">
+              <p>Sql</p>
+              <p>85%</p>
+            </div>
+            <div class="pp">
+              <div class="skillDiv">
+                <span class="skillBar sqlBar"></span>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </body>
     </>
   );
 };
