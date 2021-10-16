6811e254d549e4ffcf49739847af1fe11dcbd705
Sayaib Sarkar <66420972+sayaib@users.noreply.github.com>
2021-10-17 00:12:44 +0530

    Update Contact.jsx

--- src/webpage_component/Contact.jsx
+++ src/webpage_component/Contact.jsx
@@ -1,15 +1,40 @@
 import React from "react";
 import "./style.css";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import {  } from "react-bootstrap";
-
+import { Form, Button } from "react-bootstrap";
 
 const Contact = () => {
+  function sendEmail(e) {}
   return (
     <>
       <div id="webpage">
         <div id="webpage_body">
-         
+          <div className="container">
+            <Form onSubmit={sendEmail}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter name" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Subject</Form.Label>
+                <Form.Control type="text" placeholder="subject" />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </div>
         </div>
       </div>
     </>
